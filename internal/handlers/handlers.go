7d9d2190c5f681c7317172519062e8e33236a28b
Klaus-Dieter Schmatz <KlausDieter_Schmatz@iptiq.com>
2023-12-27 16:00:08 +0100

    Add idempotency middleware

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -10,22 +10,78 @@ func CreateInvoice() fiber.Handler {
 
 func UpdateInvoice() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		invoice := c.Params("invoice")
-		return c.SendString("update invoice " + invoice)
+		id := c.Params("id")
+		return c.SendString("update invoice " + id)
 	}
 }
 
-func UpdateLineItem() fiber.Handler {
+func RetrieveUpcomingInvoice() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		invoice := c.Params("invoice")
-		line := c.Params("line")
-		return c.SendString("update line " + line + " of invoice " + invoice)
+		customer := c.FormValue("customer")
+		return c.SendString("retrieve upcoming invoice for customer " + customer)
 	}
 }
 
 func RetrieveInvoice() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		invoice := c.Params("invoice")
-		return c.SendString("retrieve invoice " + invoice)
+		id := c.Params("id")
+		return c.SendString("retrieve invoice " + id)
+	}
+}
+
+func UpdateLineItem() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		line := c.Params("line")
+		return c.SendString("update line " + line + " of invoice " + id)
+	}
+}
+
+func RetrieveLineItems() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("retrieve line items for invoice " + id)
+	}
+}
+
+func DeleteDraftInvoice() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("delete draft invoice " + id)
+	}
+}
+
+func FinalizeInvoice() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("finalize invoice " + id)
+	}
+}
+
+func MarkInvoiceUncollectible() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("mark invoice " + id + " uncollectible")
+	}
+}
+
+func PayInvoice() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("pay invoice " + id)
+	}
+}
+
+func SendInvoice() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("send invoice " + id)
+	}
+}
+
+func VoidInvoice() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("void invoice " + id)
 	}
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/idempotency"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/tucuxi/invoices/internal/handlers"
@@ -18,14 +19,23 @@ func main() {
 	}
 
 	app.Use(
-		limiter.New(limiterConfig),
 		logger.New(),
+		limiter.New(limiterConfig),
+		idempotency.New(),
 	)
 
 	app.Post("/invoices", handlers.CreateInvoice())
-	app.Post("/invoices/:invoice", handlers.UpdateInvoice())
-	app.Post("/invoices/:invoice/lines/:line", handlers.UpdateLineItem())
-	app.Get("/invoices/:invoice", handlers.RetrieveInvoice())
+	app.Post("/invoices/:id", handlers.UpdateInvoice())
+	app.Get("/invoices/upcoming", handlers.RetrieveUpcomingInvoice()) // before GET "/invoices/:id"
+	app.Get("/invoices/:id", handlers.RetrieveInvoice())
+	app.Post("/invoices/:id/finalize", handlers.FinalizeInvoice())
+	app.Delete("/invoices/:id", handlers.DeleteDraftInvoice())
+	app.Post("/invoices/:id/mark_uncollectible", handlers.MarkInvoiceUncollectible())
+	app.Post("/invoices/:id/pay", handlers.PayInvoice())
+	app.Post("/invoices/:id/send", handlers.SendInvoice())
+	app.Post("/invoices/:id/void", handlers.VoidInvoice())
+	app.Post("/invoices/:id/lines/:line", handlers.UpdateLineItem())
+	app.Get("/invoices/:id/lines", handlers.RetrieveLineItems())
 
 	app.Listen(":3000")
 }
